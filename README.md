abc2ab1349f922d8b6f5ed5dcd3c5f25464c2326
Astik Anand <astik.anand@cotiviti.com>
2019-06-20 06:07:31 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Astik Anand
+
+Passionate Geek with love for Computer Science, Economics, Sports and Adventures.
+
+
+
+## Tech Blogs
+
+- #### Subjects
+  - ##### [Machine Learning](techblogs/machine-learning)
+
+  - ##### [Linux and Unix](techblogs/linux-unix)
+
+  - ##### [Django: Python Web Framework](techblogs/django)
+
+    
+
+- #### Articles
+
+  - ##### [Hosting static website or repository on Github using Github Pages](github-pages-boilerplate)
+
+
+
+
+
+## Non-tech Blogs
+
---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## Starting the Deployment Process
+echo -e "#################### Starting the deployment ###########################\n"
+
+## Set the error logging from here
+set -e
+
+# Change to project repo directory
+cd /Users/astik.anand/OneDrive\ -\ Cotiviti/github-website/
+
+# Build the project and only if build suceeds commit the new changes
+bundle exec jekyll build
+
+# Get the current date and time for the commit
+COMMIT_TIME=`date '+%A %d-%B-%Y at %I:%M %p'`
+
+# Commit changes and push
+git add --a
+git commit -m "Last updated on $COMMIT_TIME"
+git push
+
+## Stop the error logging
+set +e
